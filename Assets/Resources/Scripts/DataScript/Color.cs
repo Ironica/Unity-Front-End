a30286024814f6f77f5ceae753b9f34f111da1b0
emile-le-moigne <78609855+emile-le-moigne@users.noreply.github.com>
2021-05-19 15:56:25 +0200

    recuperation de fichiers

---
+++ Assets/Resources/Scripts/DataScript/Color.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+
+public enum Color
+{
+  BLACK,
+  SILVER,
+  GREY,
+  WHITE,
+  RED,
+  ORANGE,
+  GOLD,
+  PINK,
+  YELLOW,
+  BEIGE,
+  BROWN,
+  GREEN,
+  AZURE,
+  CYAN,
+  ALICEBLUE,
+  PURPLE
+}
---
+++ Assets/Resources/Scripts/DataScript/Item.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+
+public enum Item
+{
+  NONE,
+  GEM,
+  CLOSEDSWITCH,
+  OPENEDSWITCH,
+  BEEPER,
+  PORTAL,
+  PLATFORM
+}
