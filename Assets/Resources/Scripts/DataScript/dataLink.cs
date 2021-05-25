5c01ca6a61797a1cf4852b0eaddf27f0480dcfc7
emile-le-moigne <78609855+emile-le-moigne@users.noreply.github.com>
2021-05-25 16:14:51 +0200

    Map instantiation
    Creation of a map from a json file

--- Assets/Resources/Scripts/DataScript/dataLink.cs
+++ Assets/Resources/Scripts/DataScript/dataLink.cs
@@ -13,6 +13,34 @@ public class dataLink : MonoBehaviour
   private JsonBridge.DataSerialized[] payload;
   private string currentMap;
 
+  // Level 0
+  private string open0 = "Prefabs/PLAIN_OPEN_LEVEL0";
+  private string blocked0 = "Prefabs/PLAIN_HILL_LEVEL0";
+  private string water0 = "Prefabs/PLAIN_WATER";
+  private string tree0 = "Prefabs/PLAIN_TREE_LEVEL0";
+  private string desert0 = "Prefabs/PLAIN_DESERT_LEVEL0";
+  private string home0 = "Prefabs/PLAIN_HOME 1";
+  private string mountain0 = "Prefabs/PLAIN_MOUNTAIN";
+
+
+  // Level 1
+  private string open1 = "Prefabs/PLAIN_OPEN";
+  private string blocked1 = "Prefabs/PLAIN_HILL";
+  private string water1 = "Prefabs/PLAIN_WATER 1";
+  private string tree1 = "Prefabs/PLAIN_TREE";
+  private string desert1 = "Prefabs/PLAIN_DESERT";
+  private string home1 = "Prefabs/PLAIN_HOME";
+
+  // Stairs
+  private string stairLeft = "Prefabs/PLAIN_STAIRS_LEFT";
+  private string stairRight = "Prefabs/PLAIN_STAIRS_RIGHT";
+  private string stairBack = "Prefabs/PLAIN_STAIRS_BACK";
+  private string stairFront = "Prefabs/PLAIN_STAIRS_FRON";
+  private int currentStair = -1;
+
+  //Players skin
+  private string player1 = "Prefabs/ITEM/CHARACTER";
+
 
   public const string url = "http://127.0.0.1:8080/paidiki-xara";
 
@@ -59,11 +87,113 @@ public class dataLink : MonoBehaviour
 
   }
 
+  public void mapInstantiation(Block block, int level, float x, float y, int i, int j){
+    string tile;
+    if(dataObj.levels[j, i] == 1){
+      tile = tileLevel0(block);
+    }
+    else {
+      tile = tileLevel1(block);
+    }
+    GameObject tileObject  = Instantiate(Resources.Load(tile), new Vector3(x,y, 0), Quaternion.identity) as GameObject;
+    tileObject.transform.parent = this.transform;
+  }
+
+  public string tileLevel0(Block block){
+    switch(block){
+      case Block.OPEN: return open0;
+      break;
+      case Block.BLOCKED: return blocked0;
+      break;
+      case Block.WATER: return water0;
+      break;
+      case Block.TREE: return tree0;
+      break;
+      case Block.DESERT: return desert0;
+      break;
+      case Block.HOME: return home0;
+      break;
+      case Block.MOUNTAIN: return mountain0;
+      break;
+      case Block.STONE: return mountain0;
+      break;
+      case Block.LOCK: return home0;
+      break;
+      default:
+      currentStair++;
+      return stairDirection(dataObj.stairs[currentStair].dir);
+      break;
+    }
+  }
+
+  public string tileLevel1(Block block){
+    switch(block){
+      case Block.OPEN: return open1;
+      break;
+      case Block.BLOCKED: return blocked1;
+      break;
+      case Block.WATER: return water1;
+      break;
+      case Block.TREE: return tree1;
+      break;
+      case Block.DESERT: return desert1;
+      break;
+      case Block.HOME: return home1;
+      break;
+      case Block.MOUNTAIN: return mountain0;
+      break;
+      case Block.STONE: return mountain0;
+      break;
+      case Block.LOCK: return home1;
+      break;
+      default:
+      currentStair++;
+      return stairDirection(dataObj.stairs[currentStair].dir);
+      break;
+    }
+  }
+
+  private string stairDirection(Direction dir){
+    switch(dir){
+      case Direction.UP: return stairFront;
+      break;
+      case Direction.DOWN: return stairBack;
+      break;
+      case Direction.LEFT: return stairLeft;
+      break;
+      default: return stairRight;
+      break;
+    }
+  }
+  private void instantiation(){
+    float length = dataObj.grid.GetLength(1)*0.5f;
+    float heigth = dataObj.grid.GetLength(0)*0.25f;
+    GameObject tileObject1  = Instantiate(Resources.Load(player1), new Vector3(0-length,0-heigth, 0), Quaternion.identity) as GameObject;
+    tileObject1.transform.parent = this.transform;
+    GameObject tileObject2  = Instantiate(Resources.Load(player1), new Vector3(0+length,0-heigth, 0), Quaternion.identity) as GameObject;
+    tileObject2.transform.parent = this.transform;
+    GameObject tileObject3  = Instantiate(Resources.Load(player1), new Vector3(0-length,0+heigth, 0), Quaternion.identity) as GameObject;
+    tileObject3.transform.parent = this.transform;
+    GameObject tileObject4  = Instantiate(Resources.Load(player1), new Vector3(0+length,0+heigth, 0), Quaternion.identity) as GameObject;
+    tileObject4.transform.parent = this.transform;
+    float x=0f;
+    float y =0f;
+    for(int i =0; i<dataObj.grid.GetLength(1); i++){
+      x = i*0.5f;
+      y = i*0.25f*-1f;
+      for(int j = 0; j<dataObj.grid.GetLength(0); j++){
+        mapInstantiation(dataObj.grid[j,i], dataObj.levels[j,i], x, y, i, j);
+        x -=0.5f;
+        y -= 0.25f;
+      }
+    }
+  }
+
   // Start is called before the first frame update
   void Start()
   {
 
-    currentMap = "map1.json";
+    currentMap = "map2.json";
 
     dataSer = des.deserialization(pathStarterMap + currentMap);
     dataObj = new Data();
@@ -73,7 +203,7 @@ public class dataLink : MonoBehaviour
 
     string response  = des.serialization(dataSer, pathStarterMap + currentMap);
 
-
+    instantiation();
 
   }
 
