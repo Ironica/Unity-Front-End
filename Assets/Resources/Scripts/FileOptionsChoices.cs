2f021e9a01c71b659e6c0978272a30a926d107f3
Backerstrong <77734802+Backerstrong@users.noreply.github.com>
2021-05-26 14:51:07 +0200

    Choice Game Menu

--- Assets/Resources/Scripts/FileOptionsChoices.cs
+++ Assets/Resources/Scripts/FileOptionsChoices.cs
@@ -1,18 +1,24 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FileOptionsChoices : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Dropdown ChoiceDD;
     void Start()
     {
-        
-    }
+        ChoiceDD.ClearOptions();
 
-    // Update is called once per frame
-    void Update()
-    {
+        List<String> choices = new List<string>();
         
+        choices.Add("Change map");
+        choices.Add("Save file");
+        choices.Add("Load file");
+        
+        int currentIndexRes = 0;
+        ChoiceDD.AddOptions(choices);
+        ChoiceDD.value = currentIndexRes;
+        ChoiceDD.RefreshShownValue();
     }
 }
