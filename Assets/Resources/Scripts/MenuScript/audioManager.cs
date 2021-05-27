5daaa6886da9f38f8661d2abf308dd580038b168
Backerstrong <77734802+DylanDupond@users.noreply.github.com>
2021-05-27 15:00:56 +0200

    Premier ajout de music :)

--- Assets/Resources/Scripts/MenuScript/audioManager.cs
+++ Assets/Resources/Scripts/MenuScript/audioManager.cs
@@ -1,14 +1,14 @@
-/*using UnityEngine;
-
-public AudioClip[] playlist;
-public AudioSource audio;
+using UnityEngine;
 
 public class audioManager : MonoBehaviour
 {
+    public AudioClip[] playlist;
+    public AudioSource audio;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audio.clip = playlist[0];
+        audio.Play();
     }
 
     // Update is called once per frame
@@ -17,4 +17,3 @@ public class audioManager : MonoBehaviour
         
     }
 }
-*/
