e2b8289d196d561098aa18aa3ddf0737c6cb4c5b
kokoro-aya <als847a443pttfq.dhg29780yz@gmail.com>
2021-06-02 14:17:58 +0200

    added server shutdown to main menu

--- Assets/Resources/Scripts/MenuScript/MainMenu.cs
+++ Assets/Resources/Scripts/MenuScript/MainMenu.cs
@@ -1,3 +1,6 @@
+using System;
+using Resources.Scripts;
+using Resources.Scripts.DataScript;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -26,5 +29,11 @@ public class MainMenu : MonoBehaviour
     public void Credits()
     {
         
+    }
+
+    private void OnApplicationQuit()
+    {
+        var shutdownApi = "simulatte/shutdown";
+        new ShutDown(shutdownApi, Global.port).ShutDownOldServer();
     }
 }
