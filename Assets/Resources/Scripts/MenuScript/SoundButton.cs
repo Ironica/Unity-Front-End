3a79f5022f5b1d944bd4bd5155fed4468949cc6d
Ammar96399 <50889372+Ammar96399@users.noreply.github.com>
2021-06-07 09:22:07 +0200

    Final Interface

---
+++ Assets/Resources/Scripts/MenuScript/SoundButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlTypes;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Resources.Scripts.MenuScript
+{
+    public class SoundButton : MonoBehaviour
+    {
+        public GameObject soundBar; 
+        public Sprite[] soundSprites;
+        private Image soundImage;
+        private int soundState;
+
+        private void Start()
+        {
+            soundState = 0;
+            soundImage = GetComponent<Button>().image;
+            soundImage.sprite = soundSprites[soundState];
+            soundBar.SetActive(false);
+
+            gameObject.GetComponent<Button>().onClick.AddListener(TurnOnAndOff);
+        }
+
+        private void TurnOnAndOff()
+        {
+            soundState = 1 - soundState;
+            soundImage.sprite = soundSprites[soundState];
+            soundBar.SetActive(soundState != 0);
+            
+        }
+    }
+}
