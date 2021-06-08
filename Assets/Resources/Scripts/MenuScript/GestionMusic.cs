12e55f327cc40af4787b581f0c5f0b22ac1c448a
Backerstrong <77734802+DylanDupond@users.noreply.github.com>
2021-06-08 14:12:11 +0200

    Son On/Off

---
+++ Assets/Resources/Scripts/MenuScript/GestionMusic.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class GestionMusic : MonoBehaviour
+{
+    public AudioSource audio;
+
+   public void SetVolume()
+      {
+          if (audio.volume != 0f)
+          {
+              audio.volume = 0f;
+             audio.Pause();
+          }
+          else
+          {
+              audio.volume = 0.2f;
+             audio.Play();
+          }
+          
+      }
+}
