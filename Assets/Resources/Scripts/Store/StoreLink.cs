375fa9dd622d1d04c0008bfa09562f02e293d951
emile-le-moigne <78609855+emile-le-moigne@users.noreply.github.com>
2021-06-09 09:49:55 +0200

    storeLink

--- Assets/Resources/Scripts/Store/StoreLink.cs
+++ Assets/Resources/Scripts/Store/StoreLink.cs
@@ -10,15 +10,61 @@ public class StoreLink : MonoBehaviour
   private GameObject sounds;
   private GameObject themes;
 
+  private string buttonSkin = "Prefabs/STORE/BoutonPerso";
+  private string buttonSound = "Prefabs/STORE/BoutonMusicStore";
+  private string buttonTheme = "Prefabs/STORE/BoutonTheme";
+
   // Start is called before the first frame update
   void Start()
   {
     storage = JsonObjConverter.toObj();
 
     //Affichage des personnages
-    GameObject skins = GameObject.Find("Skin") as GameObject;
+    GameObject skins = GameObject.Find("Character") as GameObject;
+    GameObject sounds = GameObject.Find("Music") as GameObject;
+    GameObject themes = GameObject.Find("Themes") as GameObject;
 
+    for(int i = 0; i < storage.skins.Count; i++)
+    {
+      GameObject skinsButton = Instantiate(UnityEngine.Resources.Load(buttonSkin)) as GameObject;
+      skinsButton.transform.SetParent(skins.transform);
 
+      if(!storage.skins[i].sold){
+        skinsButton.transform.Find("PricePerso").GetComponent<Text>().text = "" + storage.skins[i].priceInGems;
+      }
+    }
+    for(int i = 0; i < storage.sounds.Count; i++)
+    {
+      GameObject soundObject = Instantiate(UnityEngine.Resources.Load(buttonSound)) as GameObject;
+      soundObject.transform.Find("").GetComponent<Text>().text = "" + storage.sounds[i].itemName;
+      GameObject price = soundObject.transform.Find("").gameObject as GameObject;
+      GameObject use = soundObject.transform.Find("").gameObject as GameObject;
+      if(!storage.sounds[i].sold)
+      {
+        price.GetComponent<Text>().text = "" + storage.sounds[i].priceInGems;
+        Destroy(use);
+      }
+      else
+      {
+        Destroy(price);
+      }
+    }
+    for(int i = 0; i < storage.themes.Count; i++)
+    {
+      GameObject themeObject = Instantiate(UnityEngine.Resources.Load(buttonTheme)) as GameObject;
+      themeObject.transform.Find("").GetComponent<Text>().text = "" + storage.themes[i].itemName;
+      GameObject price = themeObject.transform.Find("").gameObject as GameObject;
+      GameObject use = themeObject.transform.Find("").gameObject as GameObject;
+      if(!storage.themes[i].sold)
+      {
+        themeObject.transform.Find("").GetComponent<Text>().text = "" + storage.themes[i].priceInGems;
+        Destroy(use);
+      }
+      else
+      {
+        Destroy(price);
+      }
+    }
 
     JsonObjConverter.toJson(storage);
   }
