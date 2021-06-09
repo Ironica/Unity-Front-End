7c45f6545cd1514d0f52a3a36c2f039479017e40
emile-le-moigne <78609855+emile-le-moigne@users.noreply.github.com>
2021-06-09 15:54:45 +0200

    previous scene

--- Assets/Resources/Scripts/Buttons/PreviousScene.cs
+++ Assets/Resources/Scripts/Buttons/PreviousScene.cs
@@ -6,16 +6,13 @@ using UnityEngine.SceneManagement;
 public class PreviousScene : MonoBehaviour
 {
 
-    private int previousScene;
-
-    // Start is called before the first frame update
-    void Start()
+  // Update is called once per frame
+  public void goToPreviousScene(){
+    int scene = ChangeSceneManagement.getPreviousScene();
+    ChangeSceneManagement.getCurrentScene();
+    if(scene != SceneManager.GetActiveScene().buildIndex)
     {
-      previousScene = SceneManager.GetActiveScene().buildIndex-1;
+      SceneManager.LoadScene(scene);
     }
-
-    // Update is called once per frame
-    public void goToPreviousScene(){
-      SceneManager.LoadScene(0);
-    }
+  }
 }
--- Assets/Resources/Scripts/MenuScript/MainMenu.cs
+++ Assets/Resources/Scripts/MenuScript/MainMenu.cs
@@ -8,24 +8,28 @@ public class MainMenu : MonoBehaviour
     public GameObject settingsWindow;
     public void StartGame()
     {
+        ChangeSceneManagement.getCurrentScene();
         SceneManager.LoadScene("Scenes/Interface");
         StatData.setCurrent("map5");
     }
     public void Continue()
     {
-
+      ChangeSceneManagement.getCurrentScene();
     }
     public void Settings()
     {
+        ChangeSceneManagement.getCurrentScene();
         settingsWindow.SetActive(true);
     }
 
     public void CloseSettings()
     {
+        ChangeSceneManagement.getCurrentScene();
         settingsWindow.SetActive(false);
     }
     public void Credits()
     {
+      ChangeSceneManagement.getCurrentScene();
 
     }
 }
