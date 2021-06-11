ca0832abbc614ed55c9e8072a124bfda650d6216
emile-le-moigne <78609855+emile-le-moigne@users.noreply.github.com>
2021-06-11 09:27:40 +0200

    changes

---
+++ Assets/Resources/Scripts/MapMenu/Data/DataChapter.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataChapter
+{
+  public string chapterName;
+  public string mapName;
+}
---
+++ Assets/Resources/Scripts/MapMenu/Data/DataMapMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataMapMenu
+{
+  public string mapName;
+  public string story;
+  public string storyTilte;
+  public string goals;
+  public string goalsTitle;
+}
