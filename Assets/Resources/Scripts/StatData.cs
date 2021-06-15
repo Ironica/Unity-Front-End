0aea05501f3cb7e1f51afd986af6d10a23eaf260
Backerstrong <77734802+DylanDupond@users.noreply.github.com>
2021-06-15 16:01:38 +0200

    Map 2-3 et 2-4

--- Assets/Resources/Scripts/StatData.cs
+++ Assets/Resources/Scripts/StatData.cs
@@ -2,7 +2,7 @@
 
 public static class StatData
 {
-    private static string currentMap= "Chap2-1";
+    private static string currentMap= "Chap2-4";
     public static bool isPlayable { get; set; }
     public static int indexStoreMusic { get; set; }
 
