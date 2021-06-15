5424dfe77bba68338454e88011cb8d7a07e1aa54
kokoro-aya <als847a443pttfq.dhg29780yz@gmail.com>
2021-06-15 11:27:54 +0200

    update store scripting

--- Assets/Resources/Scripts/Store/TabSystem/TabButton.cs
+++ Assets/Resources/Scripts/Store/TabSystem/TabButton.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.UI; 
 using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(Image))]
 public class TabButton : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public TabGroup tabGroup;
